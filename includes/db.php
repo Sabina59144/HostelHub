4d3da052a471b008858dd1c8a0ad569e28112018
Sachintmg002 <59145@edu.nielsbrock.dk>
2026-04-30 15:47:30 +0200

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -12,12 +12,3 @@ try {
     die("Connection failed: " . $e->getMessage());
 }
 ?>
-<?php
-
-$conn = mysqli_connect("localhost","root","","hostel_db");
-
-if(!$conn){
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-?>
